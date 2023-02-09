515538a48b8864f3f41ac7142556436f5ecf027a
Kushalhk <115166395+Kushalhk@users.noreply.github.com>
2023-02-09 09:45:34 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -12,4 +12,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Origional BOT :- <a href='https://t.me/GangsterBaby_renamer_BOT'>Gangster Baby</a>\nCreater :- <a href='https://t.me/LazyDeveloper'>🦋LazyDeveloper🦋</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- KOYEB\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you **<a href='https://t.me/mRiderDM'>LazyDeveloperr</a>** for your hard work \n\n❤️ we love you <a href='https://t.me/mRiderDM'>**LazyDeveloper**</a> ❤️",quote=True)
+	await message.reply_text(f"Origional BOT :- <a href='https://t.me/Nano_Rename_bot'>Rename bot</a>\nCreater :- <a href='https://t.me/KUSHALHK'>🦋𝘾𝙧𝙚𝙖𝙩𝙤𝙧🦋</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- KOYEB\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you **<a href='https://t.me/KUSHALHK'>TG_UPDATOR</a>** for your hard work \n\n❤️ we love you <a href='https://t.me/KUSHALHK'>**KUSHALHK**</a> ❤️",quote=True)
