e6793ba44d790a836c756216fdafbaf945f72472
tamil4k <131458557+tamil4k@users.noreply.github.com>
2023-04-25 21:26:06 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -11,23 +11,23 @@ async def upgrade(bot,update):
 	⛩️INDIVIDUAL PLAN - FOR 1 MEMBER⛩️
 	
 	🥉 Bronze Tier🥉 
-	💫Daily  Upload  limit 10 GB💫
+	💫Daily  Upload  limit 5 GB💫
 	💵Price Rs 10 or 0.5$ per Month💵
 	
 	🥈 Silver Tier 🥈 
-	💫Daily  Upload  limit 25 GB💫
+	💫Daily  Upload  limit 10 GB💫
 	💵Price Rs 20 or 1$ per Month💵
 	
 	🪙 Gold Tier 🪙 
-	💫Daily  Upload  limit 40 GB💫
+	💫Daily  Upload  limit 20 GB💫
 	💵Price Rs 30 or 1.5$ per Month💵
 	
 	✨ Platinum Tier ✨ 
-	💫Daily  Upload  limit 70 GB💫
+	💫Daily  Upload  limit 40 GB💫
 	💵Price Rs 50 or 2.5$ per Month💵
 	
 	💎 Diamond Tier 💎
-	💫Daily  Upload  limit 150 GB💫
+	💫Daily  Upload  limit 100 GB💫
 	💵Price Rs 100 or 5$ per Month💵
 	
 	Pay Using Upi I'd ```karna862001-1@okicici```
@@ -37,23 +37,23 @@ async def upgrade(bot,update):
 	⛩️TEAM PLANS - FOR 1 TO 4 MEMBERS⛩️
 	
 	🥉 Bronze Tier🥉 
-	💫Daily  Upload  limit 10 GB💫
+	💫Daily  Upload  limit 5 GB💫
 	💵Price Rs 20 or 1$ per Month💵
 	
 	🥈 Silver Tier 🥈 
-	💫Daily  Upload  limit 25 GB💫
+	💫Daily  Upload  limit 10 GB💫
 	💵Price Rs 40 or 2$ per Month💵
 	
 	🪙 Gold Tier 🪙 
-	💫Daily  Upload  limit 40 GB💫
+	💫Daily  Upload  limit 20 GB💫
 	💵Price Rs 60 or 3$ per Month💵
 	
 	✨ Platinum Tier ✨ 
-	💫Daily  Upload  limit 70 GB💫
+	💫Daily  Upload  limit 40 GB💫
 	💵Price Rs 100 or 5$ per Month💵
 	
 	💎 Diamond Tier 💎
-	💫Daily  Upload  limit 150 GB💫
+	💫Daily  Upload  limit 100 GB💫
 	💵Price Rs 200 or 10$ per Month💵
 	
 	Pay Using Upi I'd ```karna862001-1@okicici```
@@ -80,23 +80,23 @@ async def upgradecm(bot,message):
 	⛩️INDIVIDUAL PLAN - FOR 1 MEMBER⛩️
 	
 	🥉 Bronze Tier🥉 
-	💫Daily  Upload  limit 10 GB💫
+	💫Daily  Upload  limit 5 GB💫
 	💵Price Rs 10 or 0.5$ per Month💵
 	
 	🥈 Silver Tier 🥈 
-	💫Daily  Upload  limit 25 GB💫
+	💫Daily  Upload  limit 10 GB💫
 	💵Price Rs 20 or 1$ per Month💵
 	
 	🪙 Gold Tier 🪙 
-	💫Daily  Upload  limit 40 GB💫
+	💫Daily  Upload  limit 20 GB💫
 	💵Price Rs 30 or 1.5$ per Month💵
 	
 	✨ Platinum Tier ✨ 
-	💫Daily  Upload  limit 70 GB💫
+	💫Daily  Upload  limit 40 GB💫
 	💵Price Rs 50 or 2.5$ per Month💵
 	
 	💎 Diamond Tier 💎
-	💫Daily  Upload  limit 150 GB💫
+	💫Daily  Upload  limit 100 GB💫
 	💵Price Rs 100 or 5$ per Month💵
 	
 	Pay Using Upi I'd ```karna862001-1@okicici```
@@ -106,23 +106,23 @@ async def upgradecm(bot,message):
 	⛩️TEAM PLANS - FOR 1 TO 4 MEMBERS⛩️
 	
 	🥉 Bronze Tier🥉 
-	💫Daily  Upload  limit 10 GB💫
+	💫Daily  Upload  limit 5 GB💫
 	💵Price Rs 20 or 1$ per Month💵
 	
 	🥈 Silver Tier 🥈 
-	💫Daily  Upload  limit 25 GB💫
+	💫Daily  Upload  limit 10 GB💫
 	💵Price Rs 40 or 2$ per Month💵
 	
 	🪙 Gold Tier 🪙 
-	💫Daily  Upload  limit 40 GB💫
+	💫Daily  Upload  limit 20 GB💫
 	💵Price Rs 60 or 3$ per Month💵
 	
 	✨ Platinum Tier ✨ 
-	💫Daily  Upload  limit 70 GB💫
+	💫Daily  Upload  limit 40 GB💫
 	💵Price Rs 100 or 5$ per Month💵
 	
 	💎 Diamond Tier 💎
-	💫Daily  Upload  limit 150 GB💫
+	💫Daily  Upload  limit 100 GB💫
 	💵Price Rs 200 or 10$ per Month💵
 	
 	Pay Using Upi I'd ```karna862001-1@okicici```
