faabc8d29e7bd19e33713db76bf65d3d53eb85eb
tamil4k <131458557+tamil4k@users.noreply.github.com>
2023-04-25 21:41:31 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -12,4 +12,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Creater :- <a href='https://t.me/ridzy96'>🦋𝘾𝙧𝙚𝙖𝙩𝙤𝙧🦋</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- HEROKU\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you **<a href='https://t.me/ridzy96'>OWNER</a>** for your hard work \n\n❤️ we love you <a href='https://t.me/ridzy96'>**RIDZY**</a> ❤️",quote=True)
+	await message.reply_text(f"Creater :- <a href='@ridzy96'>🦋𝘾𝙧𝙚𝙖𝙩𝙤𝙧🦋</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- HEROKU",quote=True)
