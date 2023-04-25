1c13560d71b5d07e2d85f814f6cfdb33fd6ed72d
tamil4k <131458557+tamil4k@users.noreply.github.com>
2023-04-25 21:30:04 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -55,7 +55,7 @@ async def ceasepremium(bot, message):
 
 @Client.on_message((filters.channel | filters.private) & filters.user(ADMIN) & filters.command(["resetpower"]))
 async def resetpower(bot, message):
-	    await message.reply_text(text=f"Do you really want to reset daily limit to default data limit 3 GB ?",quote=True,reply_markup=InlineKeyboardMarkup([
+	    await message.reply_text(text=f"Do you really want to reset daily limit to default data limit 1.2 GB ?",quote=True,reply_markup=InlineKeyboardMarkup([
 		           [InlineKeyboardButton("• YES ! Set as Default •",callback_data = "dft")],
 				   [InlineKeyboardButton("❌ Cancel ❌",callback_data = "cancel")]
 				   ]))
@@ -194,9 +194,9 @@ async def cp5(bot,update):
 async def dft(bot,update):
 	id = update.message.reply_to_message.text.split("/resetpower")
 	user_id = id[1].replace(" ", "")
-	inlimit = 3221225472
-	uploadlimit(int(user_id), 3221225472)
+	inlimit = 1288490188
+	uploadlimit(int(user_id), 1288490188)
 	usertype(int(user_id),"**Free**")
 	addpre(int(user_id))
-	await update.message.edit("Daily Data limit has been reset successsfully.\nThis account has default 3 GB renaming capacity ")
+	await update.message.edit("Daily Data limit has been reset successsfully.\nThis account has default 1.2 GB renaming capacity ")
 	await bot.send_message(user_id,"Your Daily Data limit has been reset successsfully.\n\nCheck your plan here - /myplan\n- Contact Admin 🦋<a href='https://t.me/ridzy96'>**ridzy96**</a>🦋")
